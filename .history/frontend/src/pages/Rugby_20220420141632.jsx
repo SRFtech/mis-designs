630bc09a5b7f07774cf63b2d4ec542a9739b42b3
Nash The Coder <naijeria@gmail.com>
2022-04-21 13:43:34 +0300

    added frontend folder

---
+++ .history/frontend/src/pages/Rugby_20220420141632.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Rugby() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Rugby
---
+++ frontend/src/pages/Rugby.jsx
@@ -0,0 +1,9 @@
+function Rugby() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Rugby
